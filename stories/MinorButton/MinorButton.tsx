27ad0a3f22f0f74b6f3a9f08024f7bbb6dbbc370
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-02-16 17:03:58 +0330

    Minor Button fixed

--- stories/MinorButton/MinorButton.tsx
+++ stories/MinorButton/MinorButton.tsx
@@ -6,15 +6,17 @@ export interface MinorButtonProps {
     size?: "sm" | "md" | "lg";
     variant?: "solid" | "bordered" | "light" | "flat" | "faded" | "shadow" | "ghost";
     buttonTitle?: string;
+    radius?:"none" | "sm" | "md" | "lg" | "full";
 }
 
 export const MinorButton = (props: MinorButtonProps) => {
 
-    const {size, buttonTitle, variant} = props;
+    const {size, buttonTitle, variant,radius} = props;
     return (
         <Button
             size={size}
             variant={variant}
+            radius={radius}
         >
             {buttonTitle}
         </Button>
