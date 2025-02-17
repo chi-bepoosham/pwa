ff8abc461cdb74793b83271b86d173c19b6132ae
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-02-17 14:58:17 +0330

    working on Input Component

---
+++ stories/MinorInput/MinorInput.stories.ts
@@ -0,0 +1,19 @@
+import { MinorInput } from './MinorInput';
+
+
+
+export default {
+    title: "MinorInput",
+    component: MinorInput,
+};
+
+export const Default = {
+    args: {
+
+    },
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        }
+    },
+};
---
+++ stories/MinorInput/MinorInput.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+
+export interface MinorInputProps {
+
+}
+
+export const MinorInput = (props: MinorInputProps) => {
+    const {} = props;
+
+
+    return (
+<></>
+    );
+};
