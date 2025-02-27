a2549c508022c89e1a6f277db64f7ca2416e5fa4
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-02-27 18:17:18 +0330

    working on Notification component fixed

--- stories/Notification/Notification.tsx
+++ stories/Notification/Notification.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {NotificationIcon} from "@/stories/Icons";
+import {Switch} from "@heroui/react";
 
 
 export interface NotificationProps {
@@ -10,17 +11,17 @@ export interface NotificationProps {
 export const Notification = (props: NotificationProps) => {
     const {} = props;
     return (
-        <div className="flex justify-between">
-            <div className="flex flex-row-reverse gap-2">
-                <span className="text-secondary font-semibold">
+        <div className="flex justify-between w-full mx-2">
+            <div className="flex flex-row-reverse gap-2 items-center">
+                <span className="text-secondary font-semibold select-none">
                     اعـلان‌هـای اپـلیکیشـن
                 </span>
                 <i className="text-secondary">
-                    <NotificationIcon size={25}/>
+                    <NotificationIcon size={24}/>
                 </i>
             </div>
             <div>
-
+                <Switch color="secondary"/>
             </div>
         </div>
     )
