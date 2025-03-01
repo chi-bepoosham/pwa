a6f90c6b87923f867f9d059e61e95f09662b20c0
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-03-01 21:41:57 +0330

    working on VoiceAssistant

---
+++ stories/VoiceAssistant/VoiceAssistant.stories.ts
@@ -0,0 +1,18 @@
+import {VoiceAssistant} from './VoiceAssistant';
+
+
+export default {
+    title: "VoiceAssistant",
+    component: VoiceAssistant,
+};
+
+export const Default = {
+    args: {
+
+    },
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        }
+    },
+};
---
+++ stories/VoiceAssistant/VoiceAssistant.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+
+export interface VoiceAssistantProps {
+
+}
+
+
+
+
+
+export const VoiceAssistant = (props: VoiceAssistantProps) => {
+    const {  } = props;
+    return(
+        <></>
+    )
+}
