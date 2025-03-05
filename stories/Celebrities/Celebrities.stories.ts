47bb623c5d16970fc73f8f1050ff3ad4bf2d2aba
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-03-05 15:46:37 +0330

    working on Celebrities comp[oent

---
+++ stories/Celebrities/Celebrities.stories.ts
@@ -0,0 +1,16 @@
+import {Celebrities} from './Celebrities';
+
+
+export default {
+    title: "Celebrities",
+    component: Celebrities,
+};
+
+export const Default = {
+    args: {},
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        }
+    },
+};
---
+++ stories/Celebrities/Celebrities.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export interface CelebrityProps {
+    description: string;
+}
+
+
+export const Celebrities = (props: CelebrityProps) => {
+    const {} = props;
+    return (
+        <></>
+    )
+}
