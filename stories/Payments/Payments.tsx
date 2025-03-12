8de747ead316bbefc8f922acf73f67c6e483786c
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-03-12 10:44:16 +0330

    working on Payments

--- stories/Payments/Payments.tsx
+++ stories/Payments/Payments.tsx
@@ -1,16 +1,34 @@
 import React from "react";
-
-
+import {RadioGroup, Radio} from "@heroui/radio";
 
 
 export interface PaymentsProps {
-title: string;
 }
 
 
 export const Payments = (props: PaymentsProps) => {
     const {} = props;
     return (
-        <></>
+        <RadioGroup
+            label="روش‌های پرداخت"
+            className="flex flex-col items-start justify-center gap-8"
+        >
+            <Radio
+                color="primary"
+                description="پرداخت آنلاین با تمامی کارت‌های اعتباری"
+                value="buenos-aires"
+                className="text-secondary-300"
+            >
+                پرداخت اینترنتی
+            </Radio>
+            <Radio
+                color="primary"
+                description="ویژه فروشگاه‌ها، شرکت‌ها و ..."
+                value="sydney"
+                className="text-secondary-300"
+            >
+                پرداخت با کارت اعتباری
+            </Radio>
+        </RadioGroup>
     )
 }
