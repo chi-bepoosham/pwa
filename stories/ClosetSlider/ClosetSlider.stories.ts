3629df3fa707ed1a226d2fc5c7b6de0199a456e8
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-03-14 14:36:59 +0330

    working on ClosetSLider

---
+++ stories/ClosetSlider/ClosetSlider.stories.ts
@@ -0,0 +1,19 @@
+import { ClosetSlider } from './ClosetSlider';
+
+
+
+export default {
+    title: "ClosetSlider",
+    component: ClosetSlider,
+};
+
+export const Default = {
+    args: {
+        imageUrl: "image12.png",
+    },
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        }
+    },
+};
---
+++ stories/ClosetSlider/ClosetSlider.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+export interface ClosetSliderProps {
+    imageUrl: string;
+}
+
+
+export const ClosetSlider = (props: ClosetSliderProps) => {
+    const {} = props;
+    return (
+<></>
+    )
+}
+
+
+
+
