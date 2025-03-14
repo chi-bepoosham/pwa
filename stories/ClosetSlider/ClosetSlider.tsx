d5098095a970a42a6a4718a5629a37b2e43f5705
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-03-14 20:07:05 +0330

    working on Closet

--- stories/ClosetSlider/ClosetSlider.tsx
+++ stories/ClosetSlider/ClosetSlider.tsx
@@ -9,6 +9,8 @@ export interface ClosetSliderProps {
 export const ClosetSlider = (props: ClosetSliderProps) => {
     const {} = props;
     return (
-        <></>
+        <div className="relative w-fit h-[350px] flex flex-col justify-center items-center gap-4">
+
+        </div>
     )
 }
