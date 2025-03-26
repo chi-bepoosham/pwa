d24f7b1700af1a7cded93b0500edb37caad5b22b
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-03-26 13:16:21 +0330

    working on Search component

---
+++ stories/Search/Search.stories.ts
@@ -0,0 +1,16 @@
+import {Search} from './Search';
+
+
+export default {
+    title: "Search",
+    component: Search,
+};
+
+export const Default = {
+    args: {},
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        }
+    },
+};
---
+++ stories/Search/Search.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+
+
+export interface SearchProps {
+
+}
+
+
+
+
+
+export const Search = (props: SearchProps) => {
+    const {} = props;
+    return (
+        <></>
+    )
+}
