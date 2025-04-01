8f202557c8e1faa3a913e580d9fc151d1d112a57
AminModaresi <aminiiiiiiiuii@gmail.com>
2025-04-01 14:08:57 +0330

    add font and add clsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,17 @@
-import "./globals.css";
-
+import { IRANSansX } from '@/lib/font';
+import './globals.css';
+import clsx from 'clsx';
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="fa">
-        <body
-            dir="rtl"
-            className="w-screen h-screen bg-primary-100"
-        >
-        <main className="max-w-screen-md mx-auto bg-primary h-full">
-            {children}
-        </main>
-
-        </body>
-        </html>
-    );
+  return (
+    <html lang="fa">
+      <body dir="rtl" className={clsx('w-screen h-screen bg-primary-100', IRANSansX.className)}>
+        <main className="max-w-screen-md mx-auto bg-primary h-full">{children}</main>
+      </body>
+    </html>
+  );
 }
