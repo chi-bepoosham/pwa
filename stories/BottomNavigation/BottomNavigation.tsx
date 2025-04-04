b5abb2c4a214630df812e496b7e921d3bf610ca6
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-04-04 14:30:03 +0330

    working on bottom nav

---
+++ stories/BottomNavigation/BottomNavigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export interface BottomNavigationProps {
+
+}
+
+
+export const BottomNavigation = (props: BottomNavigationProps) => {
+  const {} = props;
+  return (
+    <></>
+  );
+};
