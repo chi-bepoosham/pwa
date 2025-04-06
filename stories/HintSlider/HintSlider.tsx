c78bd6b110368e2ac8b4bc3a44813f53c40c0d6d
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-04-06 12:49:19 +0330

    working on hint slider

---
+++ stories/HintSlider/HintSlider.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export interface HintSliderProps {
+
+}
+
+
+export const HintSlider = (props: HintSliderProps) => {
+  const {} = props;
+  return (
+    <></>
+  );
+};
