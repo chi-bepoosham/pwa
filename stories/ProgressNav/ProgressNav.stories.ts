b811e11b957ac59cd274faabb5bbf9dea25fc4a9
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-04-16 13:45:56 +0330

    working on Progress Nav

---
+++ stories/ProgressNav/ProgressNav.stories.ts
@@ -0,0 +1,16 @@
+import { ProgressNav } from './ProgressNav';
+
+
+export default {
+  title: 'ProgressNav',
+  component: ProgressNav,
+};
+
+export const Default = {
+  args: {},
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
---
+++ stories/ProgressNav/ProgressNav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export interface ProgressNavProps {
+
+}
+
+
+export const ProgressNav = (props: ProgressNavProps) => {
+  const {} = props;
+  return (
+    <></>
+  );
+};
