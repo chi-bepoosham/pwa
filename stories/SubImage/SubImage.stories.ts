57a5f529d6ffe5fe6ef20ea57a409c5482cc765d
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-05-05 15:53:48 +0330

    working on subimage component

---
+++ stories/SubImage/SubImage.stories.ts
@@ -0,0 +1,16 @@
+import {SubImage} from './SubImage';
+
+
+export default {
+    title: "SubImage",
+    component: SubImage,
+};
+
+export const Default = {
+    args: {},
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        }
+    },
+};
---
+++ stories/SubImage/SubImage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export interface SubImageProps {
+}
+
+
+
+
+export const SubImage = (props: SubImageProps) => {
+const {} = props;
+    return (
+        <div>
+ </div>
+    )
+}
+
