8ad43573890b6be0c674aebd8c76bf0d991352e6
ily <99044801+ilyanozary@users.noreply.github.com>
2025-07-31 20:34:46 +0330

    Update page.tsx

--- app/(app)/profile/about/page.tsx
+++ app/(app)/profile/about/page.tsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header"
 import { Button } from "@heroui/react";
 import { ArrowRightIcon, ShoppingBagIcon } from "@/stories/Icons"
 import { useRouter } from "next/navigation"
-import Link from "next/link"
+
 import { Logo } from "@/stories/Logo";
 import { Social } from "@/stories/Social";
 
