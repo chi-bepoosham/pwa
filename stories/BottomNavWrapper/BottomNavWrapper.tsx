795ad34bc7091b0dd93a9c0dfd4e6290a929a77a
TahmineRH <tahmine.rahbar79@gmail.com>
2025-08-31 19:20:00 +0330

    refactor: Improve formatting of hideOnRoutes array and change div class to fixed for BottomNavWrapper

--- stories/BottomNavWrapper/BottomNavWrapper.tsx
+++ stories/BottomNavWrapper/BottomNavWrapper.tsx
@@ -6,12 +6,18 @@ import { BottomNavigation } from '../BottomNavigation';
 export const BottomNavWrapper = () => {
   const pathname = usePathname();
 
-  const hideOnRoutes = ['/body_types', '/profile', '/celebrities', '/profile/about','/profile/info'];
+  const hideOnRoutes = [
+    '/body_types',
+    '/profile',
+    '/celebrities',
+    '/profile/about',
+    '/profile/info',
+  ];
 
   if (hideOnRoutes.includes(pathname)) return null;
 
   return (
-    <div className="sticky w-full bottom-0 z-10">
+    <div className="fixed w-full bottom-0 z-10 ">
       <BottomNavigation />
     </div>
   );
